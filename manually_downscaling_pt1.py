d3489224b817aafba7ab80cebc8d2fb1dc80af68
Weitian Li <108146441+VTIAN14@users.noreply.github.com>
2025-01-29 21:43:08 +0000

    Update manually_downscaling_pt1.py

--- manually_downscaling_pt1.py
+++ manually_downscaling_pt1.py
@@ -4,9 +4,10 @@ input_file = "history_output.txt"
 output_file = "state_input_last.dat"
 manually_downscaling.parse_history_file(input_file, output_file)
 
-input_file = "procstat_output.txt"
+input_file1 = "procstat_output.txt"
+input_file2 = "mechanism_input.dat"
 output_file = "event_frequencies.png"
-manually_downscaling.plot_bar_chart(input_file, output_file)
+manually_downscaling.plot_bar_chart(input_file1, input_file2, output_file)
 
 input_file1 = "simulation_input.dat"
 input_file2 = "mechanism_input.dat"
